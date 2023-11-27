02665d5de86d0641a6298c3e31da991350c66c18
Pankajg3432c <120238549+Pankajg3432c@users.noreply.github.com>
2023-11-28 01:45:01 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:create_app/loginpage.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,22 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        // Application name
+        title: 'Flutter Hello World',
+        // Application theme data, you can set the colors for the application as
+        // you want
+        theme: ThemeData(
+            primarySwatch: Colors.blue, backgroundColor: Colors.amber),
+        // A widget which will be started on application startup
+        home: MyHomePage(title: "Homepage"));
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +33,71 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         // The title text which will be shown on the action bar
         title: Text(title),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Image.asset("images/login.png"),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Hi Welcome Back",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => loginpage()));
+                  },
+                  child: Text(
+                    "Go to page",
+                    style: TextStyle(fontSize: 15),
+                  ))
+            ],
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Pankaj Gupta"),
+              accountEmail: Text("pankaj10112004@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.orange,
+                child: Text(
+                  "P",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: const Text('Contact Us'),
+              onTap: () {},
+            )
+          ],
         ),
       ),
     );
